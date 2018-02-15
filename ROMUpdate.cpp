fb551dedb387de5f7dd9e94d4f35044d0cad358b
Remy Eberle <remy.eberle@amadeus.com>
2018-02-15 17:51:28 +0100

    Update NPC text conditions.

--- ROMUpdate.cpp
+++ ROMUpdate.cpp
@@ -16,14 +16,14 @@ namespace ROMUpdate {
     {
         0x183AB, /* Tool shop owner */
         0x1875E, /* Emblem A tile */
-        0x18960, /* Goat pen corner (requires goat pen) */
+        0x18960, /* Goat pen corner */
         0x19253, /* Tool shop owner's son Teddy */
         0x1983B, /* A Pass */
-        0x19914, /* Tile at end of child's secret cave (requires secret cave access) */
+        0x19914, /* Tile in child's secret cave */
         0x1A137, /* Village Chief */
         0x1A7E4, /* Magician (start of the game) */
         0x1AB78, /* Recovery Sword crystal */
-        0x1AC26, /* Grass Valley secret room crystal (text start uncertain) */
+        0x1AC26, /* Grass Valley secret room crystal */
         0x1AD15, /* Underground Castle 1st part crystal */
         0x1BE54, /* Red-Hot Mirror bird */
         0x1C116, /* Magic Bell crystal */
@@ -33,11 +33,11 @@ namespace ROMUpdate {
         0x1DC17, /* Shield Bracelet mole */
         0x1E039, /* Psycho Sword squirrel */
         0x1E1E3, /* Emblem C squirrel */
-        0x1E496, /* Water Shrine Strange Bottle (?) */
+        0x1E496, /* Water Shrine Strange Bottle */
         0x1E569, /* Light Arrow crystal */
-        0x1E4E3, /* Lost Marsh crystal (text start uncertain) */
-        0x1E537, /* Water Shrine crystal (text start uncertain, same as previous?) */
-        0x205A5, /* Mountain King (text start uncertain) */
+        0x1E4E3, /* Lost Marsh crystal */
+        0x1E537, /* Water Shrine crystal */
+        0x205A5, /* Mountain King */
         0x20D63, /* Mushroom Shoes boy */
         0x210C1, /* Nome */
         0x21A6E, /* Emblem E snail */
@@ -167,8 +167,8 @@ namespace ROMUpdate {
                     /* If the NPC doesn't normally give EXP, let us turn the
                        prize into a Medical Herb for now. */
                     ROMFile.seekp (ItemAddress, ios::beg);
-                    Byte = 0x38;
-                    ROMFile.write((char*)(&Byte), 1);
+                    ItemID = MEDICAL_HERB;
+                    ROMFile.write((char*)(&ItemID), 1);
                 }
             }
             else {
@@ -186,6 +186,108 @@ namespace ROMUpdate {
                 else {
                     ROMFile.seekp (ItemAddress, ios::beg);
                     ROMFile.write((char*)(&ItemID), 1);
+                }
+            }
+
+            if (ItemID != GEMS_EXP) {
+                /* For some NPCs we need to update their text conditions, otherwise it might
+                   not give the wished item if the player already has its original item. */
+                switch (i) {
+                case ITEM_TOOL_SHOP_OWNER:
+                    ROMFile.seekp (0x1839B, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
+                case ITEM_TEDDY:
+                    ROMFile.seekp (0x1922E, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
+                case ITEM_BIRD_RED_HOT_MIRROR:
+                    ROMFile.seekp (0x1BE3D, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    ROMFile.seekp (0x1BE44, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
+                case ITEM_CRYSTAL_MAGIC_BELL:
+                    ROMFile.seekp (0x1C0C6, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
+                case ITEM_MOLE_SHIELD_BRACELET:
+                    ROMFile.seekp (0x1DC07, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
+                case ITEM_SQUIRREL_PSYCHO_SWORD:
+                    ROMFile.seekp (0x1E01C, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
+                case ITEM_MOUNTAIN_KING:
+                    ROMFile.seekp (0x203C1, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    ROMFile.seekp (0x204DF, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    ROMFile.seekp (0x2058A, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    ROMFile.seekp (0x20635, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
+                case ITEM_BOY_MUSHROOM_SHOES:
+                    ROMFile.seekp (0x20D53, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
+                case ITEM_CHEST_OF_DRAWERS_MYSTIC_ARMOR:
+                    ROMFile.seekp (0x22A6B, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
+                case ITEM_PLANT_HERB:
+                    ROMFile.seekp (0x22BB1, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
+                case ITEM_PLANT_ACTINIDIA_LEAVES:
+                    ROMFile.seekp (0x2319B, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
+                case ITEM_MOUSE_SPARK_BOMB:
+                    ROMFile.seekp (0x238E6, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
+                case ITEM_SUPER_BRACELET:
+                    ROMFile.seekp (0x24983, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
+                case ITEM_QUEEN_MAGRIDD:
+                    ROMFile.seekp (0x249A1, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
+                case ITEM_SOLDIER_PLATINUM_CARD:
+                    ROMFile.seekp (0x24BEF, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
+                case ITEM_MAID_HERB:
+                    ROMFile.seekp (0x24E83, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
+                case ITEM_MERMAID_HERB:
+                    ROMFile.seekp (0xF8305, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
+                case ITEM_MERMAID_MAGIC_FLARE:
+                    ROMFile.seekp (0xF9087, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
+                case ITEM_MERMAID_RED_HOT_STICK:
+                    ROMFile.seekp (0xF985A, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    ROMFile.seekp (0xF9861, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
+                case ITEM_LUE:
+                    ROMFile.seekp (0xF9BB7, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    /* Modify text conditions for Mermmaid guarding Lue's prison entrance */
+                    ROMFile.seekp (0xF98FE, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    ROMFile.seekp (0xF99D3, ios::beg);
+                    ROMFile.write((char*)(&ItemID), 1);
+                    break;
                 }
             }
         }
