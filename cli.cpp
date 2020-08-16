3cbc49e3c6326df42213411e1392d20d0612fad3
Florent Castelli <florent.castelli@gmail.com>
2020-08-16 21:53:41 +0200

    Update command line parameters to support race mode

--- cli.cpp
+++ cli.cpp
@@ -1,23 +1,33 @@
 #include "Randomizer.h"
 #include "Random.h"
 
+#include <optional>
 #include <stdlib.h>
 
 #define ROM_FILE_NAME      "Soul Blazer (U) [!].smc"
 #define MOD_ROM_FILE_NAME  "Soul Blazer Randomized.smc"
 
 int main (int argc, char** argv ) {
-    const char *in_file = ROM_FILE_NAME;
-    unsigned int seed;
-    if(argc > 1) {
-        in_file = argv[1];
-    }
-    if(argc > 2) {
-        seed = atoi(argv[2]);
-    } else {
-        seed = Random::RandomInit(0);
+    std::string in_file;
+    std::string out_file;
+    std::optional<unsigned int> seed;
+    Randomizer::Options options;
+
+    for (int i = 1; i < argc; i++) {
+       if (in_file.empty()) {
+           in_file = argv[i];
+       } else if (out_file.empty()) {
+           out_file = argv[i];
+       } else if (!seed.has_value()) {
+           seed = atoi(argv[i]);
+       } else {
+           options = Randomizer::Options(argv[i]);
+       }
     }
 
-    Randomizer::Options options;
-    return Randomizer::Randomize(in_file, MOD_ROM_FILE_NAME, seed, options) ? 0 : 1;
+    if (in_file.empty()) in_file = ROM_FILE_NAME;
+    if (out_file.empty()) out_file = MOD_ROM_FILE_NAME;
+    if (!seed.has_value()) seed = Random::RandomInit(0);
+
+    return Randomizer::Randomize(in_file, out_file, seed.value(), options) ? 0 : 1;
 }
